d1339fd1df1ecbbd470009b5c75314fd6878b0ba
Luciano Diamand <lucianodiamand@gmail.com>
2022-06-26 20:28:04 -0300

    Project init

---
+++ app/src/main/java/ar/com/diamand/android/example/MainActivity.java
@@ -0,0 +1,30 @@
+package ar.com.diamand.android.example;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import android.support.v4.content.ContextCompat;
+import android.support.v4.app.ActivityCompat;
+import android.content.pm.PackageManager;
+
+import java.util.HashMap;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
